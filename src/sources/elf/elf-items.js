b1b2456cfb45bc66ca11242e3897c576c9a87686
Zxc02042244 <qaz41327@gmail.com>
2026-07-07 08:21:36 +0800

    feat: add verified Elf item list

--- src/sources/elf/elf-items.js
+++ src/sources/elf/elf-items.js
@@ -226,6 +226,474 @@ export const ELF_MARKET_COVERAGE_ITEMS = [
     name: "Milk",
     group: "Resource",
     category: "Animal Product"
+  },
+  {
+    itemId: 305002,
+    name: "CiDi Echo",
+    group: "Outfit",
+    category: "Pi Elf Outfit"
+  },
+  {
+    itemId: 305005,
+    name: "Tiger Head",
+    group: "Outfit",
+    category: "Pi Elf Outfit"
+  },
+  {
+    itemId: 305012,
+    name: "Genesis Pioneer",
+    group: "Outfit",
+    category: "Pi Elf Outfit"
+  },
+  {
+    itemId: 305042,
+    name: "Cosmic Sovereign",
+    group: "Outfit",
+    category: "Pi Elf Outfit"
+  },
+  {
+    itemId: 305049,
+    name: "Take My Pi",
+    group: "Outfit",
+    category: "Pi Elf Outfit"
+  },
+  {
+    itemId: 15,
+    name: "Gem Gift Card",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2000,
+    name: "Damaged Treasure Map",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2101,
+    name: "Leather Hydration Bag",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2102,
+    name: "Turpentine Torch",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2103,
+    name: "Skinning Knife",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2104,
+    name: "Animal Skin Quiver",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2105,
+    name: "Hunting Horn",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2106,
+    name: "Brass Lantern",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2107,
+    name: "Whetstone",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2108,
+    name: "Sealed Coarse Salt Shaker",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2109,
+    name: "Flint",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2110,
+    name: "Bundle Of Linen Rope",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2111,
+    name: "Hand Drawn Area Sketch",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2112,
+    name: "Paint Sealed Letter",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2113,
+    name: "Dark Camouflage Cloak",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2114,
+    name: "Shabby Metal Sign",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2115,
+    name: "Bronze Pocket Watch",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2116,
+    name: "Complete Antlers",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2117,
+    name: "Old Pipe",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2118,
+    name: "Portable Wine Bottle",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2119,
+    name: "Animal Tooth Necklace",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2120,
+    name: "Animal Bone Dice",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2201,
+    name: "Bronze Telescope",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2202,
+    name: "Forged Seal",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2203,
+    name: "Nautical Compass",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2204,
+    name: "Crytal Monocle",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2205,
+    name: "Portable Balance",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2206,
+    name: "Precision Hourglass",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2207,
+    name: "Brass Astrolabe",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2208,
+    name: "Exquisite Nautical Charts",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2209,
+    name: "Seiko Magnifying Glass",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 2210,
+    name: "Music Box",
+    group: "General",
+    category: "General"
+  },
+  {
+    itemId: 201001,
+    name: "Rootbud Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201002,
+    name: "Earthbud Clasp",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201003,
+    name: "Vine Clip",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201004,
+    name: "Greenbud Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201005,
+    name: "Rosy Chain",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201006,
+    name: "Sweetroot Clasp",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201007,
+    name: "Grain Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201008,
+    name: "Sungrain Drop",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201101,
+    name: "Seedling Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201102,
+    name: "Fieldbloom Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201103,
+    name: "Rootbloom Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201104,
+    name: "Soil Clasp",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201105,
+    name: "Vinebloom Clip",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201106,
+    name: "Venrdant Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201107,
+    name: "Rosy Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201108,
+    name: "Rootleaf Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201801,
+    name: "Goldgrain Pin",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 201802,
+    name: "Sungrain Charm",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 202001,
+    name: "Budwood Charm",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 202002,
+    name: "Whiteleaf Charm",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 202101,
+    name: "Pebble Charm",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 202401,
+    name: "Vigor Charm",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 202501,
+    name: "Fortune Charm",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203001,
+    name: "Rootbud Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203002,
+    name: "Earthbud Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203003,
+    name: "Vine Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203004,
+    name: "Greenbud Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203005,
+    name: "Rosy Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203006,
+    name: "Sweetroot Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203007,
+    name: "Grain Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203008,
+    name: "Sungrain Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203101,
+    name: "Seedling Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203102,
+    name: "Field Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203801,
+    name: "Workshop Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203802,
+    name: "Kitchen Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 203803,
+    name: "Bakery Book",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 204001,
+    name: "Starbell",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 204002,
+    name: "Field Lamp",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 204003,
+    name: "Grove Lamp",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 204004,
+    name: "Mine Lamp",
+    group: "Blueprint",
+    category: "Blueprint"
+  },
+  {
+    itemId: 204005,
+    name: "Herd Bell",
+    group: "Blueprint",
+    category: "Blueprint"
   }
 ];
 
